80f8ded9d8d0637ca2b124f9d2188f05ed04e32e
Huan Hock Lim <huanhocklim@104-242.priv23.nus.edu.sg>
2017-02-07 11:38:26 +0800

    Extract commonalities from Phone, Email and Address classes into a parent class called Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,27 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Contact parent class in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class Contact {
+
+    protected String value;
+    protected boolean isPrivate;
+    
+
+    public String toString() {
+        return value;
+    }
+
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
+   
